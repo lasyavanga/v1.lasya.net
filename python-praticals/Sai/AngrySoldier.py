6aed5e8544a9a98eadca832bf0e8a47ed74ab188
Lasya Vanga <ToLasyaVanga@gmail.com>
2021-05-02 17:44:30 -0700

    Classes

---
+++ python-praticals/Sai/AngrySoldier.py
@@ -0,0 +1,29 @@
+class AngrySoldier:
+    print(input("What is your Favorite Animal? "))
+    print(input("Do you like Imagination? "))
+    print("""Well... 
+    If you said 'Yes' to 
+    'Do you like Imagination?',
+    Then, Here You Go!""")
+    print("""
+              \/      |---------------------------|
+             (||)     |                           |
+             (  )     } Fruit Head                |
+            ((  ))    |                           |
+           (((  )))   |                           |
+          ((((  ))))  |                           |
+          ||  \ /  ||                             |
+          ||o <=> o||                             |
+      |---||-_-_-_-||---|                         |
+      |   {  <<->>  }   |                         } Angry Soldier
+      |   | <|   |> |   |                         |
+     (|)  [   \ /   ]  (|)                        |
+      V   /  || ||  \\   V  } Razor Sharp Drillers |
+         ||---------||                            |
+           |  ||  |                               |
+           |  ||  |                               |
+        ___|__||__|___                            |
+       |___|__<>__|___|---------------------------|
+              \/
+            Angry Person Controlling The "Angry Soldier" 
+    """)
---
+++ python-praticals/Sai/Ligatures.py
@@ -0,0 +1,33 @@
+class Ligatures:
+    print("""
+    Ligatures in Fira Code:
+        Common:
+            Arthimetics:
+                ++
+                --
+                /==
+                &&
+                ||
+                ||=
+            Scope:
+                ->
+                =>
+                ::
+                __
+            Equalitiy:
+                ==
+                ===
+                !=
+                !==
+                <=
+                >=
+            Comprasions:
+                
+            Comments:
+                
+            Escaped Chars:
+                
+            Bit Operations:
+                
+            Hexadecimal Ex:
+    """)
