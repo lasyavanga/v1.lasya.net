7a008a11ffdb82d983600c4efb135fdc82d9cb45
Lasya Vanga <tolasyavanga@gmail.com>
2021-06-23 18:38:09 -0700

    Added Clicking Game

--- js/nav.js
+++ js/nav.js
@@ -1,76 +1,80 @@
 const links = [
-    {
-        linkName: "Home",
-        link: "/index.html",
-    },
-    {
-        linkName: "About",
-        link: "/pages/about.html",
-    },
-    {
-        linkName: "Education",
-        link: "/pages/education.html",
-    },
-    {
-        linkName: "Links",
-        link: "/pages/links.html",
-    },
-    {
-        linkName: "Photos",
-        link: "/pages/photos.html",
-    },
-    {
-        linkName: "Drawings",
-        link: "/pages/drawings.html",
-    },
-    {
-        linkName: "Piano",
-        link: "/pages/piano.html",
-    },
-    {
-        linkName: "Places Visited",
-        link: "/pages/places-visited.html",
-    },
-    {
-        linkName: "HTML Practicals",
-        link: "/pages/html-practicals",
-    },
-    {
-        linkName: "CSS Practicals",
-        link: "/pages/css-practicals/",
-    },
-    {
-        linkName: "About Github",
-        link: "/pages/how-git-works.html",
-    },
-    {
-        linkName: "Contact",
-        link: "/pages/contact.html",
-    },
+  {
+    linkName: "Home",
+    link: "/index.html",
+  },
+  {
+    linkName: "About",
+    link: "/pages/about.html",
+  },
+  {
+    linkName: "Education",
+    link: "/pages/education.html",
+  },
+  {
+    linkName: "Links",
+    link: "/pages/links.html",
+  },
+  {
+    linkName: "Photos",
+    link: "/pages/photos.html",
+  },
+  {
+    linkName: "Drawings",
+    link: "/pages/drawings.html",
+  },
+  {
+    linkName: "Piano",
+    link: "/pages/piano.html",
+  },
+  {
+    linkName: "Places Visited",
+    link: "/pages/places-visited.html",
+  },
+  {
+    linkName: "HTML Practicals",
+    link: "/pages/html-practicals",
+  },
+  {
+    linkName: "CSS Practicals",
+    link: "/pages/css-practicals/",
+  },
+  {
+    linkName: "About Github",
+    link: "/pages/how-git-works.html",
+  },
+  {
+    linkName: "Clicking Game",
+    link: "/pages/html-js-practicals/clicking-game.html",
+  },
+  {
+    linkName: "Contact",
+    link: "/pages/contact.html",
+  },
 ];
 
 (function () {
-    const fragment = document.createDocumentFragment();
+  const fragment = document.createDocumentFragment();
 
-    for (let i = 0; i < links.length; i++) {
-        const nav = document.getElementById("nav-links");
-        // Create and set attributes for a
-        const a = document.createElement("a");
-
-        a.textContent = links[i].linkName;
-
-        // Create li
-        const li = document.createElement("li");
-
-        a.href = links[i].link;
-
-        // Append a in li
-        li.appendChild(a);
-
-        // Append li in nav
-        fragment.appendChild(li);
-    }
-
+  for (let i = 0; i < links.length; i++) {
     const nav = document.getElementById("nav-links");
-    nav.appendChild(fragment);
+    // Create and set attributes for a
+    const a = document.createElement("a");
+
+    a.textContent = links[i].linkName;
+
+    // Create li
+    const li = document.createElement("li");
+
+    a.href = links[i].link;
+
+    // Append a in li
+    li.appendChild(a);
+
+    // Append li in nav
+    fragment.appendChild(li);
+  }
+
+  const nav = document.getElementById("nav-links");
+  nav.appendChild(fragment);
 })();
