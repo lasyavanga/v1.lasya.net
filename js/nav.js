d52db24c8b18b7cb87fd712ec7f45e0d4f34f091
Lasya Vanga <tolasyavanga@gmail.com>
2021-06-26 14:23:48 -0700

    Pretterized

--- js/nav.js
+++ js/nav.js
@@ -1,80 +1,80 @@
 const links = [
-  {
-    linkName: "Home",
-    link: "/index.html?v=1",
-  },
-  {
-    linkName: "About",
-    link: "/pages/about.html?v=1",
-  },
-  {
-    linkName: "Education",
-    link: "/pages/education.html?v=1",
-  },
-  {
-    linkName: "Links",
-    link: "/pages/links.html?v=1",
-  },
-  {
-    linkName: "Photos",
-    link: "/pages/photos.html?v=1",
-  },
-  {
-    linkName: "Drawings",
-    link: "/pages/drawings.html?v=1",
-  },
-  {
-    linkName: "Piano",
-    link: "/pages/piano.html?v=1",
-  },
-  {
-    linkName: "Places Visited",
-    link: "/pages/places-visited.html?v=1",
-  },
-  {
-    linkName: "HTML Practicals",
-    link: "/pages/html-practicals?v=1",
-  },
-  {
-    linkName: "CSS Practicals",
-    link: "/pages/css-practicals/?v=1",
-  },
-  {
-    linkName: "About Github",
-    link: "/pages/how-git-works.html?v=1",
-  },
-  {
-    linkName: "Clicking Game",
-    link: "/pages/html-js-practicals/clicking-game.html?v=1",
-  },
-  {
-    linkName: "Contact",
-    link: "/pages/contact.html?v=1",
-  },
+    {
+        linkName: "Home",
+        link: "/index.html?v=1",
+    },
+    {
+        linkName: "About",
+        link: "/pages/about.html?v=1",
+    },
+    {
+        linkName: "Education",
+        link: "/pages/education.html?v=1",
+    },
+    {
+        linkName: "Links",
+        link: "/pages/links.html?v=1",
+    },
+    {
+        linkName: "Photos",
+        link: "/pages/photos.html?v=1",
+    },
+    {
+        linkName: "Drawings",
+        link: "/pages/drawings.html?v=1",
+    },
+    {
+        linkName: "Piano",
+        link: "/pages/piano.html?v=1",
+    },
+    {
+        linkName: "Places Visited",
+        link: "/pages/places-visited.html?v=1",
+    },
+    {
+        linkName: "HTML Practicals",
+        link: "/pages/html-practicals?v=1",
+    },
+    {
+        linkName: "CSS Practicals",
+        link: "/pages/css-practicals/?v=1",
+    },
+    {
+        linkName: "About Github",
+        link: "/pages/how-git-works.html?v=1",
+    },
+    {
+        linkName: "Clicking Game",
+        link: "/pages/html-js-practicals/clicking-game.html?v=1",
+    },
+    {
+        linkName: "Contact",
+        link: "/pages/contact.html?v=1",
+    },
 ];
 
 (function () {
-  const fragment = document.createDocumentFragment();
+    const fragment = document.createDocumentFragment();
 
-  for (let i = 0; i < links.length; i++) {
+    for (let i = 0; i < links.length; i++) {
+        const nav = document.getElementById("nav-links");
+        // Create and set attributes for a
+        const a = document.createElement("a");
+
+        a.textContent = links[i].linkName;
+
+        // Create li
+        const li = document.createElement("li");
+
+        a.href = links[i].link;
+
+        // Append a in li
+        li.appendChild(a);
+
+        // Append li in nav
+        fragment.appendChild(li);
+    }
+
     const nav = document.getElementById("nav-links");
-    // Create and set attributes for a
-    const a = document.createElement("a");
-
-    a.textContent = links[i].linkName;
-
-    // Create li
-    const li = document.createElement("li");
-
-    a.href = links[i].link;
-
-    // Append a in li
-    li.appendChild(a);
-
-    // Append li in nav
-    fragment.appendChild(li);
-  }
-
-  const nav = document.getElementById("nav-links");
-  nav.appendChild(fragment);
+    nav.appendChild(fragment);
 })();
--- js/piano.js
+++ js/piano.js
@@ -34,8 +34,7 @@ const iframeAttrs = {
     src: "https://www.youtube.com/embed/",
     title: "YouTube video player",
     frameborder: "0",
-    allow:
-        "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
+    allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
     allowfullscreen: "allowfullscreen",
 };
 
